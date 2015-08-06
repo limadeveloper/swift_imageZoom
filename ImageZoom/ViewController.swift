fb2ee063ef46230bb8f4f40adf1af42d4bad5754
John Silva <Johncarlosboy@gmail.com>
2015-08-06 08:09:02 -0300

    initial commit

---
+++ ImageZoom/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  ImageZoom
+//
+//  Created by John Silva on 8/6/15.
+//  Copyright © 2015 John Lima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIScrollViewDelegate {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        scrollView.minimumZoomScale = 1.0
+        scrollView.maximumZoomScale = 5.0
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+
+
+}
+
